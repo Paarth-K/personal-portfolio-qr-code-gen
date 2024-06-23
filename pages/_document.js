4069a3da826a485b9d2f69ce266e754c635e72e7
Paarth Kukreja <paarthkukreja@outlook.com>
2024-06-23 13:00:59 +0400

    added clarity

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          src="/clarity.js"
+          strategy={"beforeInteractive"}
+          async
+          type="text/javascript"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
